e84674d40dfa380831c176f57ac2e77152c9f85e
Heinti <olegukraine33@gmail.com>
2023-04-02 23:40:21 +0300

    123w

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import css from './Footer.module.css'
 const Footer = () => {
     return (
       <div className={css.footer}>
-        Copyright &copy; 2023. Created by Herasymenko Valentyn.
+        Copyright &copy; 2023. Created by heint.
       </div>
     );
   };
